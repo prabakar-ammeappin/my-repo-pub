6875c314036032b8f47435076c3988bca3ea014f
prabakar-ammeappin <57911690+prabakar-ammeappin@users.noreply.github.com>
2019-11-26 16:06:43 +0100

    Update: README.md first-file notebooks

--- notebooks/Users/prabakar.ammeappin@databricks.com/DB-git-integ.py
+++ notebooks/Users/prabakar.ammeappin@databricks.com/DB-git-integ.py
@@ -1,3 +1,11 @@
 # Databricks notebook source
+# MAGIC %sh 
+# MAGIC echo "# my-repo" >> README.md
+# MAGIC git init
+# MAGIC git add README.md
+# MAGIC git commit -m "first commit"
+# MAGIC git remote add origin git@github.com:prabakar-ammeappin/my-repo.git
+# MAGIC git push -u origin master
 
-// Adding test line
+# COMMAND ----------
+
